d1a3a72ed5da5582ec7fc0d00bf7c63b506c6064
Tejas Pisal <137098887+pisaltejas@users.noreply.github.com>
2024-06-13 21:40:34 +0530

    Update and rename 13 June to 12 June | Count number containing 4.py

---
+++ 12 June | Count number containing 4.py
@@ -0,0 +1,22 @@
+
+class Solution:
+    def countNumberswith4(self, n : int) -> int:
+        # code here
+        
+
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+
+        n = int(input())
+
+        obj = Solution()
+        res = obj.countNumberswith4(n)
+
+        print(res)
+
+# } Driver Code Ends
