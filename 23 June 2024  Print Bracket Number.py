b87f051148c7292c382edb8271f0a26be98eaf18
Tejas Pisal <137098887+pisaltejas@users.noreply.github.com>
2024-06-23 21:15:47 +0530

    Create 23 June 2024  Print Bracket Number.py

---
+++ 23 June 2024  Print Bracket Number.py
@@ -0,0 +1,32 @@
+
+class Solution:
+	def bracketNumbers(self, str):
+		# code here
+		stack = []
+        result = []
+        bracket_number = 1
+
+        for char in str:
+            if char == '(':
+                stack.append(bracket_number)
+                result.append(bracket_number)
+                bracket_number += 1
+            elif char == ')':
+                result.append(stack.pop())
+            # Skip adding non-bracket characters to result
+            # else:
+            #     result.append(-1)
+
+        return result
+
+
+
+
+
+
+
+  
+
+#tb
+
+#https://youtu.be/h_C1AizZNOk?si=LUHdeV2gISjOmSvO
