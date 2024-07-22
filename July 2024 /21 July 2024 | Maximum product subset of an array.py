c6e246d8ce667a8936090c3fd727f1e7fbd04399
Tejas Pisal <137098887+pisaltejas@users.noreply.github.com>
2024-07-22 19:44:58 +0530

    21 July 2024 | Maximum product subset of an array.py

--- July 2024 /21 July 2024 | Maximum product subset of an array.py
+++ July 2024 /21 July 2024 | Maximum product subset of an array.py
@@ -1,28 +1,69 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
 class Solution:
     def findMaxProduct(self, arr):
-        # code edutechbarsha
-         MOD = 10**9 + 7
-        prod = 1
-        maxi = float('-inf')
-        zero_count = 0
-        negative_count = 0
         
-        for num in arr:
-            if num != 0:
-                prod = (prod * num) % MOD
-                if num < 0:
-                    negative_count += 1
-                    maxi = max(maxi, num)
-            else:
-                zero_count += 1
+        n=len(arr)
+        max_neg=float("-inf")
+        count_zero=0
+        count_neg=0
+        prod=1
         
-        if zero_count == len(arr):
+        mod=10**9+7
+        
+        if n==1:
+            return arr[0]
+        
+        for i in range(0,len(arr)):
+            
+            if arr[i]==0:
+                count_zero+=1
+                continue
+            
+            if arr[i]<0:
+                max_neg=max(max_neg,arr[i])
+                count_neg+=1
+                
+            prod=(prod*arr[i]) % mod
+            
+        if count_zero==n:
             return 0
-        
-        if negative_count % 2 != 0:
-            if negative_count == 1 and zero_count > 0 and negative_count + zero_count == len(arr):
+            
+        if count_neg % 2 !=0:
+            
+            if (count_neg==1 and count_zero>0) and (count_neg+count_zero==n):
                 return 0
-            prod = (prod * pow(maxi, MOD-2, MOD)) % MOD
-        
-        return prod % MOD
-#tb
+            
+            
+            prod = (prod * pow(max_neg, mod - 2, mod)) % mod 
+            
+        return prod
+
+
+ # Driver Code Starts.
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+
+    index = 0
+    t = int(data[index])
+    index += 1
+    results = []
+    
+    for _ in range(t):
+        n = int(data[index])
+        index += 1
+        arr = list(map(int, data[index:index + n]))
+        index += n
+        solution = Solution()
+        ans = solution.findMaxProduct(arr)
+        results.append(ans)
+    
+    for result in results:
+        print(result)
+# } Driver Code Ends
